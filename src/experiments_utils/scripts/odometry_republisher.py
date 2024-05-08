e04676a1968b6a054bcf0f4fb39f3794425e2536
iQuad427 <quentin.roels@gmail.com>
2024-05-08 16:47:12 +0200

    preparation for rosbag

---
+++ src/experiments_utils/scripts/odometry_republisher.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# Script by Saïd
+
+import rospy
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from math import sin, cos, atan2
+
+
+class MessageRepublisher:
+    def __init__(self):
+        rospy.init_node('message_republisher', anonymous=True)
+        self.imu_subscriber = rospy.Subscriber('rvr/imu', Imu, self.imu_callback)
+        self.imu_publisher = rospy.Publisher('imu', Imu, queue_size=10)
+        self.odom_subscriber = rospy.Subscriber('/rvr/odom', Odometry, self.odom_callback)
+        self.odom_publisher = rospy.Publisher('/rvr/odom/correct', Odometry, queue_size=10)
+        self.odom_imu_mixed_publisher = rospy.Publisher('/rvr/odom/odom_imu_mixed', Odometry, queue_size=10)
+        self.initial_orientation = None
+        self.initial_yaw = None
+        self.initial_x = None
+        self.initial_y = None
+
+        self.last_orientation = None
+
+    def imu_callback(self, imu_msg):
+        # Modify the frame_id field to "base_link"
+        imu_msg.header.frame_id = "base_link"
+        # Publish the modified IMU message
+        self.imu_publisher.publish(imu_msg)
+
+        # Store the initial orientation
+        if self.initial_orientation is None:
+            self.initial_orientation = imu_msg.orientation
+
+        self.last_orientation = imu_msg.orientation
+
+    def odom_callback(self, odom_msg):
+        if self.initial_orientation is not None:
+            if self.initial_yaw is None:
+                if self.initial_x is None and self.initial_y is None:
+                    self.initial_x = odom_msg.pose.pose.position.x
+                    self.initial_y = odom_msg.pose.pose.position.y
+                else:
+                    delta_x = odom_msg.pose.pose.position.x - self.initial_x
+                    delta_y = odom_msg.pose.pose.position.y - self.initial_y
+                    yaw1 = atan2(delta_y, delta_x)
+                    (roll, pitch, yaw) = euler_from_quaternion([
+                        self.initial_orientation.x,
+                        self.initial_orientation.y,
+                        self.initial_orientation.z,
+                        self.initial_orientation.w
+                    ])
+                    # print("yaw1 : ", yaw1)
+                    # print("yaw : ", yaw)
+                    if (delta_x != 0 and delta_y != 0):
+                        self.initial_yaw = yaw - yaw1
+            else:
+                # Calculate new x, y positions based on initial orientation
+                x = odom_msg.pose.pose.position.x
+                y = odom_msg.pose.pose.position.y
+                new_x = x * cos(self.initial_yaw) - y * sin(self.initial_yaw)
+                new_y = x * sin(self.initial_yaw) + y * cos(self.initial_yaw)
+                # Modify the frame_id and child_frame_id fields
+                odom_msg.header.frame_id = "odom"
+                odom_msg.child_frame_id = "base_link"
+                # Update the x, y positions
+                odom_msg.pose.pose.position.x = new_x
+                odom_msg.pose.pose.position.y = new_y
+                # Publish the modified odometry message
+                self.odom_publisher.publish(odom_msg)
+
+                # Publish the odometry message with IMU orientation mixed in
+                odom_msg.pose.pose.orientation = self.last_orientation
+                self.odom_imu_mixed_publisher.publish(odom_msg)
+
+    def run(self):
+        rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        message_republisher = MessageRepublisher()
+        message_republisher.run()
+    except rospy.ROSInterruptException:
+        pass
--- src/experiments_utils/scripts/sensor_measurement.py
+++ src/experiments_utils/scripts/sensor_measurement.py
@@ -10,93 +10,31 @@ from experiments_utils.msg import Manage, Distance, Distances
 
 from utils import DWM, parse_distances
 
-stop = False
-
-
-def callback(msg):
-    global stop
-
-    if msg.stop:
-        stop = True
-
-
-def signal_handler(sig, frame):
-    global stop
-    stop = True
-
 
 def talker():
-    simulation = True if sys.argv[1] == "True" else False
-
-    resp_module_port = sys.argv[2]
-    init_module_port = sys.argv[3]
-
-    output_dir = sys.argv[4]
-    output_file = sys.argv[5]
-
-    # if output file already exists, raise an error
-    if not simulation:
-        try:
-            with open(f"{output_dir}/{output_file}", "x") as f:
-                pass
-        except FileExistsError:
-            raise ValueError("ERROR: Output file already exists, stopping experiment to avoid deleting data")
+    module_type = sys.argv[1]
+    module_port = sys.argv[2]
 
     rospy.init_node('sensor_measurement', anonymous=True)
 
-    # Subscribe to the manager command (to stop the node when needed)
-    rospy.Subscriber('manager_command', Manage, callback)
-
     # Publish the read distances
-    publisher = rospy.Publisher(f'sensor_read', Distances, queue_size=1000)
+    publisher = rospy.Publisher(f'/{module_type}/sensor_read', Distances, queue_size=1000)
 
-    if not simulation:
-        try:
-            resp_module = DWM(port=resp_module_port)
-            init_module = DWM(port=init_module_port)
-        except serial.SerialException as e:
-            raise ValueError("Couldn't open serial port", e)
+    try:
+        module = DWM(port=module_port)
+    except serial.SerialException as e:
+        raise ValueError("Couldn't open serial port", e)
 
-        with open(f"{output_dir}/{output_file}", "w+") as f:
-            start = datetime.datetime.now()
+    while not rospy.is_shutdown():
+        line: str = module.read()
 
-            while not rospy.is_shutdown() and not stop:
-                resp_line: str = resp_module.read()
-                init_line: str = init_module.read()
+        msg, faulty_frame = parse_distances(line)
 
-                timestamp = (datetime.datetime.now() - start).total_seconds()
-
-                f.write(f"{timestamp}&{init_line}\n{timestamp}&{resp_line}\n")
-                for line in [init_line, resp_line]:
-                    msg, faulty_frame = parse_distances(line)
-
-                    if not faulty_frame:
-                        publisher.publish(msg)
-    else:
-        with open(f"{output_dir}/{output_file}", "r") as f:
-            # Read file, line by line and output only if timestamp is reached
-            lines = f.readlines()
-
-        start = datetime.datetime.now()
-
-        for line in tqdm.tqdm(lines):
-            timestamp, distances = line.split("&")
-
-            msg, faulty_frame = parse_distances(distances)
-            if not faulty_frame:
-                # While not the right moment, do nothing
-                while (datetime.datetime.now() - start).total_seconds() < float(timestamp):
-                    pass
-
-                publisher.publish(msg)
-            if stop:
-                break
+        if not faulty_frame:
+            publisher.publish(msg)
 
 
 if __name__ == '__main__':
-    # Set signal handler
-    signal.signal(signal.SIGINT, signal_handler)
-
     try:
         talker()
     except rospy.ROSInterruptException:
